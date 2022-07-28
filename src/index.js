ea6d81d38ca8d96b597eb5d961852b375e3f2ef3
AlexeiKozlovskiy <gekleberefin@gmail.com>
2022-07-28 20:37:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+
+    let decoded =  expr.split('**********').map(a => {
+        let newWord = '';
+        for (let i = 0; i < a.length; i = i + 10) {
+            let morse = '';
+            let letter = a.slice(i, i+10);
+            // console.log(letter)
+            for (let k = letter.length-1; k >= 0; k = k - 2) {
+                if ((letter[k-1] + letter[k]) === '11') {
+                    morse = '-' + morse;
+                }
+                if ((letter[k-1] + letter[k]) === '10') {
+                    morse = '.' + morse;
+                }      
+            }
+            console.log(morse)
+            newWord += MORSE_TABLE[morse];
+        }
+        
+        return newWord;
+    });
+    return decoded.join(' ');}
 
 module.exports = {
     decode
